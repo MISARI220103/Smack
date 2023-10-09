85aa27a11f8cc0460c61f87599cab537e49469b6
misari <misari220103@gmail.com>
2023-10-09 23:41:27 +0530

    lesson 4

--- app/src/main/java/com/example/smack_2/LoginActivity.kt
+++ app/src/main/java/com/example/smack_2/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.example.smack_2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.content.Intent
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,7 +13,8 @@ class LoginActivity : AppCompatActivity() {
 
     fun loginCreateUserBtnClicked(view:View)
     {
-
+        val createUserIntent=Intent(this,CreateUserActivity::class.java)
+        startActivity(createUserIntent)
     }
     fun loginLoginBtnClicked(view:View)
     {
